ca395ad486e72f80113b4363fca78deda40105c5
Hisamitsu Okabe <okabe@worksap.co.jp>
2018-04-20 17:51:44 +0900

    feat: csrf

--- app.js
+++ app.js
@@ -9,9 +9,28 @@ $(function () {
                 // console.log('### response=' + JSON.stringify(response));
                 $('#api-result-body').text(JSON.stringify(response.body));
                 $('#api-result-cookie').text(JSON.stringify(response.cookie.filter(val => val.includes('csrf'))));
+                addCsrfInQuery(response.cookie.filter(val => val.includes('csrf'))[0]);
             }
         });
 
         return false;
     });
+
+    function addCsrfInQuery(str) {
+        var csrf = getCsrfKey(str);
+        $('a').each(function() {
+            const current = $(this).attr('href');
+            $(this).attr('href', (current + '&csrfKey=' + csrf));
+            console.log('##1=' + $(this).attr('href'));
+        });
+    };
+    function getCsrfKey(str) {
+        let params = {};
+        str.split('; ').forEach(function(val) {
+            const pair = val.split('=');
+            params[pair[0]] = pair[1];
+        });
+        return params['x-wap-ec-csrf'];
+    }
+
 });
